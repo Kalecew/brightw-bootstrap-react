c340f2a2d11f608a312bc5f4adddecfc012e2d7f
Kalecew <adamlovelacee@gmail.com>
2022-01-17 15:05:57 +0300

    finish

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,4 +1,3 @@
-import react from 'react' 
 import './style.css' 
 import { Theme } from '../../components'
 
@@ -9,24 +8,24 @@ const Main = () => (
         <p className="fs-2 text-center intro-text lh-sm mb-5 text-secondary">Welcome! This is <span>Bright</span>, a very clean Wordpress psd file theme. That makes it easy to showcase your work</p>
 
         <ul className="themes-list list-unstyled row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-5">
-            <Theme theme={title: 'Keira', autor: 'inaikas', img:'theme1.jpg'}/>
-            <Theme theme={title: 'SRK', autor: 'theme_ocean', img:'theme2.jpg' }/>
-            <Theme theme={title: 'EduPro', autor: 'gonthemes', img:'theme3.jpg' }/>
-            <Theme theme={title: 'EightyDays', autor: 'GretaThemes', img:'theme4.jpg' }/>
-            <Theme theme={title: 'Lily', autor: 'AqThemes', img:'theme5.jpg' }/>
-            <Theme theme={title: 'Daisy', autor: 'AqThemes', img:'theme6.jpg' }/>
-            <Theme theme={title: 'Nutritia', autor: 'YetiTeam', img:'theme7.jpg' }/>
-            <Theme theme={title: 'Techup', autor: 'YetiTeam', img:'theme8.jpg' }/>
-            <Theme theme={title: 'Kanter', autor: 'BrainiakThemes', img:'theme9.jpg' }/>
-            <Theme theme={title: 'Moreno', autor: 'Brickthemes', img:'theme10.jpg' }/>
-            <Theme theme={title: 'Homeswort', autor: 'RockThemes', img:'theme11.jpg' }/>
-            <Theme theme={title: 'Angel', autor: 'iamAbdus', img:'theme12.jpg' }/>
+            <Theme theme={{title: 'Keira', autor: 'inaikas', img:'files/theme1.jpg'}}/>
+            <Theme theme={{title: 'SRK', autor: 'theme_ocean', img:'files/theme2.jpg' }}/>
+            <Theme theme={{title: 'EduPro', autor: 'gonthemes', img:'files/theme3.jpg' }}/>
+            <Theme theme={{title: 'EightyDays', autor: 'GretaThemes', img:'files/theme4.jpg' }}/>
+            <Theme theme={{title: 'Lily', autor: 'AqThemes', img:'files/theme5.jpg' }}/>
+            <Theme theme={{title: 'Daisy', autor: 'AqThemes', img:'files/theme6.jpg' }}/>
+            <Theme theme={{title: 'Nutritia', autor: 'YetiTeam', img:'files/theme7.jpg' }}/>
+            <Theme theme={{title: 'Techup', autor: 'YetiTeam', img:'files/theme8.jpg' }}/>
+            <Theme theme={{title: 'Kanter', autor: 'BrainiakThemes', img:'files/theme9.jpg' }}/>
+            <Theme theme={{title: 'Moreno', autor: 'Brickthemes', img:'files/theme10.jpg' }}/>
+            <Theme theme={{title: 'Homeswort', autor: 'RockThemes', img:'files/theme11.jpg' }}/>
+            <Theme theme={{title: 'Angel', autor: 'iamAbdus', img:'files/theme12.jpg' }}/>
         </ul>
 
         <div className="mt-5" aria-label="Навигация по страницам">
           <ul className="pagination justify-content-center">
             <li className="page-item disabled">
-              <a className="page-link text-black-50" href="#" aria-label="Предыдущая"  tabindex="-1" aria-disabled="true">
+              <a className="page-link text-black-50" href="#" aria-label="Предыдущая"  tabIndex="-1" aria-disabled="true">
                 <span aria-hidden="true">&laquo;</span>
               </a>
             </li>
--- src/components/theme/index.js
+++ src/components/theme/index.js
@@ -1,14 +1,13 @@
-import react from 'react' 
 import './style.css' 
 
-const Theme = (theme) => (
-    <li class="col">
-      <div class="card h-100 border-0">              
-        <div class="card-body text-center">
-          <a href="#" class="card-title text-secondary h4 text-decoration-none">${theme.title}</a>
-          <p class="card-text text-black-50">от ${theme.autor}</p>
+const Theme = ({ theme }) => (
+    <li className="col">
+      <div className="card h-100 border-0">              
+        <div className="card-body text-center">
+          <a href="#" className="card-title text-secondary h4 text-decoration-none">{theme.title}</a>
+          <p className="card-text text-black-50">от {theme.autor}</p>
         </div>
-        <a href="#"><img src="files/${theme.img}" width="400" height="250" class="card-img-bottom" alt="..."/></a>
+        <a href="#"><img src={theme.img} width="400" height="250" className="card-img-bottom" alt="..."/></a>
       </div>
     </li>
 )
